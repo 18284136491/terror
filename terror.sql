c64e5c36cae8ab82ffc08927b21e4e31d66bf911
lucky <499820942@qq.com>
2018-04-28 18:50:28 +0800

    sql

--- terror.sql
+++ terror.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50553
 File Encoding         : 65001
 
-Date: 2018-04-19 21:28:10
+Date: 2018-04-28 18:49:48
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,63 +21,20 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `commodity`;
 CREATE TABLE `commodity` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL COMMENT '商品名',
-  `describe` varchar(255) NOT NULL COMMENT '商品描述',
-  `price` decimal(10,2) unsigned zerofill NOT NULL COMMENT '商品价格',
-  `repertory` int(2) NOT NULL COMMENT '商品库存',
-  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '商品状态 0下架 1出售 ',
-  `pic` varchar(80) DEFAULT NULL COMMENT '商品主图片',
-  `pic2` varchar(80) DEFAULT NULL COMMENT '图片2',
-  `pic3` varchar(80) DEFAULT NULL COMMENT '图片3',
-  `pic4` varchar(80) DEFAULT NULL COMMENT '图片4',
-  `pic5` varchar(80) DEFAULT NULL COMMENT '图片5',
-  `music` varchar(80) DEFAULT NULL COMMENT '商品背景音乐',
+  `name` varchar(20) NOT NULL COMMENT '标题',
+  `content` varchar(255) DEFAULT NULL COMMENT '描述',
+  `pic` varchar(80) NOT NULL COMMENT '图片',
+  `music` varchar(80) NOT NULL COMMENT '背景音乐',
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态 0下架 1上架',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='商品表';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='terror商品表';
 
 -- ----------------------------
 -- Records of commodity
 -- ----------------------------
-INSERT INTO `commodity` VALUES ('2', '暴龙眼镜', '', '00004200.35', '0', '1', null, null, null, null, null, null);
-INSERT INTO `commodity` VALUES ('3', '雪花眼镜', '', '00003500.00', '0', '1', null, null, null, null, null, null);
-
--- ----------------------------
--- Table structure for commodity_type
--- ----------------------------
-DROP TABLE IF EXISTS `commodity_type`;
-CREATE TABLE `commodity_type` (
-  `commodityid` int(10) unsigned NOT NULL COMMENT '商品id',
-  `typeid` int(10) unsigned NOT NULL COMMENT '商品类型id'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='商品-商品类型中间表';
-
--- ----------------------------
--- Records of commodity_type
--- ----------------------------
-INSERT INTO `commodity_type` VALUES ('2', '1');
-INSERT INTO `commodity_type` VALUES ('2', '3');
-INSERT INTO `commodity_type` VALUES ('3', '4');
-
--- ----------------------------
--- Table structure for member
--- ----------------------------
-DROP TABLE IF EXISTS `member`;
-CREATE TABLE `member` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `phone` char(11) NOT NULL COMMENT '手机',
-  `password` char(32) NOT NULL COMMENT '密码',
-  `username` varchar(30) NOT NULL COMMENT '用户名',
-  `titleimg` varchar(100) DEFAULT NULL COMMENT '用户头像',
-  `money` decimal(10,2) DEFAULT NULL COMMENT '金币',
-  `integral` int(10) NOT NULL COMMENT '积分',
-  `referrerid` tinyint(1) DEFAULT NULL COMMENT '推荐人id',
-  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态 0禁用 1启用',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='用户表';
-
--- ----------------------------
--- Records of member
--- ----------------------------
-INSERT INTO `member` VALUES ('5', '18284136491', '2e3b923363a360d190d40056f6f60b3b', '默momo', null, null, '0', null, '1');
+INSERT INTO `commodity` VALUES ('1', '测试', '从前有座山', 'https://www.baidu.com/img/bd_logo1.png?qua=high', '', '1');
+INSERT INTO `commodity` VALUES ('2', '测试22', '从前有座山', 'https://www.baidu.com/img/bd_logo1.png?qua=high', '', '1');
+INSERT INTO `commodity` VALUES ('3', '测试33', '从前有座山', 'https://www.baidu.com/img/bd_logo1.png?qua=high', '', '1');
 
 -- ----------------------------
 -- Table structure for menu
@@ -141,36 +98,18 @@ DROP TABLE IF EXISTS `token`;
 CREATE TABLE `token` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `url` varchar(80) NOT NULL COMMENT 'url地址',
-  `token` varchar(50) NOT NULL COMMENT 'token签名',
-  `key` varchar(30) NOT NULL COMMENT 'token对应的key',
-  `status` tinyint(1) NOT NULL COMMENT 'token状态 0禁用 1启用',
+  `token` varchar(40) NOT NULL COMMENT 'token签名',
+  `key` varchar(20) NOT NULL COMMENT 'token对应的key',
+  `status` tinyint(1) NOT NULL COMMENT 'token状态 0未使用 1已使用',
   `time` int(10) NOT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `token` (`token`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='token表';
 
 -- ----------------------------
 -- Records of token
 -- ----------------------------
 INSERT INTO `token` VALUES ('1', 'www.terror.com/index.php/index/token/b3c273cadd-471a64-b990dd-dd934262a1ae', 'b3c273cadd-471a64-b990dd-dd934262a1ae', '5ad610372a342', '1', '1523978295');
-
--- ----------------------------
--- Table structure for type
--- ----------------------------
-DROP TABLE IF EXISTS `type`;
-CREATE TABLE `type` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `typename` varchar(30) NOT NULL COMMENT '类型名',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='商品类型表';
-
--- ----------------------------
--- Records of type
--- ----------------------------
-INSERT INTO `type` VALUES ('1', '热门');
-INSERT INTO `type` VALUES ('2', '推荐');
-INSERT INTO `type` VALUES ('3', '新款');
-INSERT INTO `type` VALUES ('4', '经典');
-INSERT INTO `type` VALUES ('5', '测试');
 
 -- ----------------------------
 -- Table structure for user
